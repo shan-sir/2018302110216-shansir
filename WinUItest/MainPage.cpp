7b0cee96292b027857657905731a1823d4c661d4
Your Name <you@example.com>
2020-10-31 22:17:33 +0800

    initial commit

---
+++ WinUItest/MainPage.cpp
@@ -0,0 +1,32 @@
+﻿#include "pch.h"
+#include "MainPage.h"
+#include "MainPage.g.cpp"
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winrt::WinUItest::implementation
+{
+    MainPage::MainPage()
+    {
+        InitializeComponent();
+    }
+
+    int32_t MainPage::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainPage::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainPage::myButton_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        myButton().Content(box_value(L"Clicked"));
+    }
+}
---
+++ packages/Microsoft.WinUI.3.0.0-preview2.200713.0/readme.txt
@@ -0,0 +1,3 @@
+Thanks for installing the Windows UI Library (WinUI) NuGet package!
+
+See our release notes: https://aka.ms/winui3releasenotes
